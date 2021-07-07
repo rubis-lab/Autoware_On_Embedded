49b6e57ded327691d97aa93b4af921512a2a0790
HayeonP <kite9240@gmail.com>
2021-07-07 18:48:16 +0900

    sched_c is updated

---
+++ autoware.ai/src/autoware/common/rubis_sched/src/sched_c.c
@@ -0,0 +1,255 @@
+#include "rubis_sched/sched_c.h"
+
+int key_id_;
+int is_scheduled_;
+int gpu_scheduling_flag_;
+GPUSchedInfo* gpu_sched_info_;
+int gpu_scheduler_pid_;
+char* task_filename_;
+char* gpu_deadline_filename_;
+// unsigned long long gpu_deadline_list_[1024];
+unsigned long long* gpu_deadline_list_;
+int max_gpu_id_ = 0;
+
+// system call hook to call SCHED_DEADLINE
+int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags){
+	return syscall(__NR_sched_setattr, pid, attr, flags);
+}
+
+int sched_getattr(pid_t pid, struct sched_attr *attr, unsigned int size, unsigned int flags)
+{
+	return syscall(__NR_sched_getattr, pid, attr, size, flags);
+}
+
+int set_sched_deadline(int _tid, __u64 _exec_time, __u64 _deadline, __u64 _period) {
+    struct sched_attr attr;
+    attr.size = sizeof(attr);
+    attr.sched_flags = 0;
+    attr.sched_nice = 0;
+    attr.sched_priority = 0;
+
+    attr.sched_policy = SCHED_DEADLINE; // 6
+    attr.sched_runtime = _exec_time;
+    attr.sched_deadline = _deadline;
+    attr.sched_period = _period;
+
+    int ret = sched_setattr(_tid, &attr, attr.sched_flags);
+    if(ret < 0) {
+        printf("[ERROR] sched_setattr failed. Are you root? (%d)\n", ret);
+        perror("sched_setattr");
+        exit(-1);
+        return 0;
+    } 
+    return 1;
+}
+
+void request_task_scheduling(double task_minimum_inter_release_time, double task_execution_time, double task_relative_deadline){
+  set_sched_deadline(gettid(), 
+    (u_int64_t)(task_execution_time), 
+    (u_int64_t)(task_relative_deadline), 
+    (u_int64_t)(task_minimum_inter_release_time)
+  );
+}
+
+void yield_task_scheduling(){
+  sched_yield();
+}
+
+void init_gpu_scheduling(char* task_filename, char* gpu_deadline_filename, int key_id){
+  printf("init_gpu_scheduling\n");
+  gpu_scheduling_flag_ = 1;
+
+  task_filename_ = (char *)malloc(strlen(task_filename) * sizeof(char));
+  gpu_deadline_filename_ = (char *)malloc(strlen(gpu_deadline_filename) * sizeof(char));
+
+  strcpy(task_filename, task_filename);
+  strcpy(gpu_deadline_filename_, gpu_deadline_filename);
+  key_id_ = key_id;
+
+  // Get deadlines
+  printf("deadline filename: %s\n", gpu_deadline_filename_);
+  get_deadline_list();
+
+  // Init signal handler
+  signal(SIGINT, sig_handler);
+  signal(SIGTSTP, sig_handler);
+  signal(SIGQUIT, sig_handler);
+
+  // Create task file
+  FILE* task_fp;
+  task_fp = fopen(task_filename_, "w");
+  if(task_fp == NULL){
+      printf("Cannot create task file at %s\n", task_filename_);
+      exit(1);
+  }
+  fprintf(task_fp, "%d\n", getpid());
+  fprintf(task_fp, "%d", key_id_);
+  fclose(task_fp);
+
+  // Get pid of scheduler
+  FILE* scheduler_fp;
+  printf("Wait the scheduler...\n");
+
+  while(1){
+      scheduler_fp = fopen("/tmp/np_edf_scheduler", "r");
+      if(scheduler_fp) break;
+  }
+  while(1){
+      fscanf(scheduler_fp, "%d", &gpu_scheduler_pid_);
+      if(gpu_scheduler_pid_ != 0) break;
+  }
+  printf("Scheduler pid: %d\n", gpu_scheduler_pid_);
+  fclose(scheduler_fp);
+
+
+  // Initialize scheduling information (shared memory data)
+  FILE* sm_key_fp;
+  sm_key_fp = fopen("/tmp/sm_key", "r");
+  if(sm_key_fp == NULL){
+      printf("Cannot open /tmp/sm_key\n");
+      termination();
+  }
+
+  key_t key;
+  int shmid;
+  key = ftok("/tmp/sm_key", key_id_);
+  shmid = shmget(key, sizeof(GPUSchedInfo), 0666|IPC_CREAT);
+  gpu_sched_info_ = (GPUSchedInfo*)shmat(shmid, 0, 0);
+  gpu_sched_info_->pid = getpid();
+  gpu_sched_info_->state = NONE;
+  gpu_sched_info_->scheduling_flag = 0;
+  printf("Task [%d] is ready to work\n", getpid());
+
+  
+  return;
+}
+
+void sig_handler(int signum){
+  if(signum == SIGINT || signum == SIGTSTP || signum == SIGQUIT){
+    termination();
+  }
+}
+
+void get_deadline_list(){
+  char gpu_deadline_filename[RUBIS_SCHED_BUFFER_SIZE];
+  char separator[2] = "/";
+  char* user_name = getenv("USER_HOME");
+
+  if(gpu_deadline_filename_[0] != '~'){
+    strcpy(gpu_deadline_filename, gpu_deadline_filename_);
+  }
+  else{
+    strcpy(gpu_deadline_filename, user_name);
+    strcat(gpu_deadline_filename, separator);
+    strcat(gpu_deadline_filename, &gpu_deadline_filename_[1]);
+  }  
+
+  FILE* fp;
+  fp = fopen(gpu_deadline_filename, "r");
+  if(fp==NULL){
+	  fprintf(stderr, "Cannot find file %s\n", gpu_deadline_filename);
+	  exit(1);
+  }
+  char buf[1024];
+  
+  while(1){
+    int id;
+    if(!fgets(buf, 1024, fp)) break;
+    strtok(buf, "\n");
+    sscanf(buf, "%d, %*llu", &id);
+    if(id > max_gpu_id_) max_gpu_id_ = id;
+  }
+
+  gpu_deadline_list_ = (unsigned long long *)malloc(sizeof(unsigned long long) * (max_gpu_id_+1));
+  printf("file read is finished\n");
+
+  rewind(fp);
+  while(1){    
+    int id;
+    long long int deadline;
+    if(!fgets(buf, 1024, fp)) break;
+    sscanf(buf, "%d, %llu", &id, &deadline);
+    gpu_deadline_list_[id] = deadline;
+  }
+  fclose(fp);
+
+  return;  
+}
+
+void termination(){
+  printf("TERMINATION\n");
+	if(gpu_scheduling_flag_==1){
+		gpu_sched_info_->state = STOP;
+  	shmdt(gpu_sched_info_);
+	}
+  
+  free(gpu_deadline_list_);
+  if(remove(task_filename_)){
+      printf("Cannot remove file %s\n", task_filename_);
+      exit(1);
+  }
+
+  if(task_profiling_flag_){
+    fclose(task_response_time_fp_);
+  }
+
+  if(gpu_profiling_flag_){
+    fclose(seg_response_time_fp_);
+    fclose(seg_execution_time_fp_);
+    free(task_filename_);
+    free(gpu_deadline_filename_);
+  }
+
+  exit(0);
+}
+
+void request_gpu(unsigned int id){  
+  stop_profiling_cpu_seg_response_time();
+  if(gpu_scheduling_flag_==1){
+    if(id > max_gpu_id_){
+      printf("[ERROR] GPU segment id bigger than max segment id!\n");
+      exit(1);
+    }
+    
+    unsigned long long relative_deadline = gpu_deadline_list_[id];
+    gpu_sched_info_->deadline = get_current_time_ns() + relative_deadline;
+    gpu_sched_info_->state = WAIT;
+  }
+  
+  start_profiling_gpu_seg_response_time();
+
+  if(gpu_scheduling_flag_ == 1){
+    while(1){
+      kill(gpu_scheduler_pid_, SIGUSR1);
+      if(gpu_sched_info_->scheduling_flag == 1) break;
+    }
+  }
+
+  start_profiling_gpu_seg_execution_time();
+
+  if(gpu_scheduling_flag_ == 1){
+    gpu_sched_info_->state = RUN;
+    gpu_sched_info_->deadline = -1;
+  }
+}
+
+void yield_gpu(unsigned int id){
+  if(gpu_scheduling_flag_==1){
+    gpu_sched_info_->scheduling_flag = 0;
+    gpu_sched_info_->state = NONE;
+  }
+  stop_profiling_cpu_seg_response_time();
+  stop_profiling_gpu_seg_time(id);
+  start_profiling_cpu_seg_response_time();
+}
+
+
+void yield_gpu_with_remark(unsigned int id, const char* remark){
+  if(gpu_scheduling_flag_==1){
+    gpu_sched_info_->scheduling_flag = 0;
+    gpu_sched_info_->state = NONE;
+  }
+  stop_profiling_cpu_seg_response_time();
+  stop_profiling_gpu_seg_time_with_remark(id, remark);
+  start_profiling_cpu_seg_response_time();
+}
---
+++ autoware.ai/src/autoware/common/rubis_sched/src/sched_profiling_c.c
@@ -0,0 +1,149 @@
+#include "rubis_sched/sched_profiling_c.h"
+
+int task_profiling_flag_;
+int gpu_profiling_flag_;
+
+FILE* task_response_time_fp_;
+FILE* seg_execution_time_fp_;
+FILE* seg_response_time_fp_;
+
+unsigned int cpu_seg_id_;
+int is_gpu_profiling_started_;
+
+struct timespec task_start_time_;
+struct timespec task_end_time_;
+unsigned long long gpu_seg_response_time_;
+unsigned long long gpu_seg_execution_time_;
+unsigned long long cpu_seg_response_time_;
+
+void init_task_profiling(char* _task_response_time_filename){
+  char task_response_time_filename[RUBIS_SCHED_BUFFER_SIZE];
+  char* user_name = getenv("USER_HOME");
+  char separator[2] = "/";
+
+  if(_task_response_time_filename[0] != '~'){
+    strcpy(task_response_time_filename, _task_response_time_filename);
+  }
+  else{      
+    strcpy(task_response_time_filename, user_name);
+    strcat(task_response_time_filename, separator);
+    strcat(task_response_time_filename, &_task_response_time_filename[1]);
+  }
+
+  task_profiling_flag_ = 1;
+  task_response_time_fp_ = fopen(task_response_time_filename, "w+");
+  chmod(task_response_time_filename, strtol("0777", 0, 8));
+}
+
+void start_task_profiling(){
+  if(task_profiling_flag_) clock_gettime(CLOCK_MONOTONIC, &task_start_time_);
+}
+
+void stop_task_profiling(){
+  if(task_profiling_flag_){
+    clock_gettime(CLOCK_MONOTONIC, &task_end_time_);
+    fprintf(task_response_time_fp_, "%lld.%.9ld,%lld.%.9ld,%d\n",(long long)task_start_time_.tv_sec, task_start_time_.tv_nsec, (long long)task_end_time_.tv_sec, task_end_time_.tv_nsec, getpid());
+    fflush(task_response_time_fp_);
+  }
+}
+
+void init_gpu_profiling(char* _execution_time_filename, char* _response_time_filename){    
+  char execution_time_filename[RUBIS_SCHED_BUFFER_SIZE];    
+  char response_time_filename[RUBIS_SCHED_BUFFER_SIZE];    
+  char separator[2] = "/";
+  char* user_name = getenv("USER_HOME");
+
+
+  if(_execution_time_filename[0] != '~'){
+    strcpy(execution_time_filename, _execution_time_filename);
+  }
+  else{      
+    strcpy(execution_time_filename, user_name);
+    strcat(execution_time_filename, separator);
+    strcat(execution_time_filename, &_execution_time_filename[1]);
+  }
+
+  if(_response_time_filename[0] != '~'){
+    strcpy(response_time_filename, _response_time_filename);
+  }
+  else{      
+    strcpy(response_time_filename, user_name);
+    strcat(response_time_filename, separator);
+    strcat(response_time_filename, &_response_time_filename[1]);
+  }
+
+  gpu_profiling_flag_ = 1;
+  seg_execution_time_fp_ = fopen(execution_time_filename, "w+");
+  chmod(execution_time_filename, strtol("0777", 0, 8));
+  fprintf(seg_execution_time_fp_, "ID, TIME, REMARK\n");
+
+  seg_response_time_fp_ = fopen(response_time_filename, "w+");
+  chmod(response_time_filename, strtol("0777", 0, 8));
+  fprintf(seg_response_time_fp_, "ID, TIME, REMARK\n");
+  cpu_seg_id_ = 0;
+}
+
+void start_profiling_cpu_seg_response_time(){
+  if(gpu_profiling_flag_)
+    cpu_seg_response_time_ = get_current_time_ns();
+}
+
+void stop_profiling_cpu_seg_response_time(){
+  if(gpu_profiling_flag_){
+    cpu_seg_response_time_ = get_current_time_ns() - cpu_seg_response_time_;
+    fprintf(seg_response_time_fp_, "cpu_%u, %llu,  \n", cpu_seg_id_, cpu_seg_response_time_);
+    cpu_seg_id_++;
+  }    
+}
+
+void start_profiling_gpu_seg_response_time(){
+  if(gpu_profiling_flag_) gpu_seg_response_time_ = get_current_time_ns();
+}
+
+void start_profiling_gpu_seg_execution_time(){
+  if(gpu_profiling_flag_) gpu_seg_execution_time_ = get_current_time_ns();
+}
+
+void stop_profiling_gpu_seg_time(unsigned int id){
+  if(gpu_profiling_flag_){
+    unsigned long long current_time = get_current_time_ns();
+    gpu_seg_response_time_ = current_time - gpu_seg_response_time_;
+    gpu_seg_execution_time_ = current_time - gpu_seg_execution_time_;
+    
+    fprintf(seg_response_time_fp_, "gpu_%u, %llu\n", id, gpu_seg_response_time_);
+    fprintf(seg_execution_time_fp_, "gpu_%u, %llu\n", id, gpu_seg_execution_time_);
+  }
+}
+
+void stop_profiling_gpu_seg_time_with_remark(unsigned int id, const char* remark){
+  if(gpu_profiling_flag_){
+    unsigned long long current_time = get_current_time_ns();
+    gpu_seg_response_time_ = current_time - gpu_seg_response_time_;
+    gpu_seg_execution_time_ = current_time - gpu_seg_execution_time_;
+    
+    fprintf(seg_response_time_fp_, "gpu_%u, %llu, %s\n", id, gpu_seg_response_time_, remark);
+    fprintf(seg_execution_time_fp_, "gpu_%u, %llu, %s\n", id, gpu_seg_execution_time_, remark);
+  }
+}
+
+unsigned long long get_current_time_ns(){
+  struct timespec ts;
+  unsigned long long current_time;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  current_time = ts.tv_sec%10000 * 1000000000 + ts.tv_nsec;
+  return current_time;
+}
+
+void refresh_gpu_profiling(){
+  if(is_gpu_profiling_started_ == 0) is_gpu_profiling_started_ = 1;
+  else if(is_gpu_profiling_started_ == 1){
+    stop_profiling_cpu_seg_response_time();
+    fprintf(seg_response_time_fp_, "-1, -1, -1\n");
+    fprintf(seg_execution_time_fp_, "-1, -1, -1\n");
+    
+    fflush(seg_response_time_fp_);
+    fflush(seg_execution_time_fp_);
+    cpu_seg_id_ = 0;
+    start_profiling_cpu_seg_response_time();
+  }
+}
