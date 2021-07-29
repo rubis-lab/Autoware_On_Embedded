da106029d921ef090dffe6d6beb81e5ddcfd68a5
Spiraline <snowbaepbird@gmail.com>
2021-07-29 16:39:42 +0900

    Change some parameter in 138

--- autoware.ai/autoware_files/keyboard_controller.py
+++ autoware.ai/autoware_files/keyboard_controller.py
@@ -6,8 +6,8 @@ from geometry_msgs.msg import TwistStamped
 ACC_MIN = -0.2
 ACC_MAX = 0.2
 PI = 3.141592
-STEER_MIN = -PI/2
-STEER_MAX = PI/2
+STEER_MIN = -PI/3*2
+STEER_MAX = PI/3*2
 
 current_pressed = set()
 global current_acc
