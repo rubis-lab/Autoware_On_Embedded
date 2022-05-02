e768dd3416ceada7333d58bb10e7257312c0eb9f
rubis-lab <rubis.chief@gmail.com>
2022-05-02 16:05:29 +0900

    base_link TF bug in gnss_pose_pub is fixed

--- rubis_ws/src/gnss_converter/src/gnss_pose_pub.cpp
+++ rubis_ws/src/gnss_converter/src/gnss_pose_pub.cpp
@@ -24,46 +24,30 @@ typedef sync_policies::ExactTime<inertiallabs_msgs::gps_data, inertiallabs_msgs:
 
 static ros::Publisher gnss_pose_pub_;
 static double x_offset_, y_offset_, yaw_offset_;
+static double roll_, pitch_, yaw_;
+static geometry_msgs::PoseStamped gnss_pose_;
 
 void gnss_pose_pub_cb(const inertiallabs_msgs::gps_data::ConstPtr &msg_gps, const inertiallabs_msgs::ins_data::ConstPtr &msg_ins){
-    static tf::TransformBroadcaster br;
+    gnss_pose_.header = msg_gps->header;
+    gnss_pose_.header.frame_id = "/map";
 
-    geometry_msgs::PoseStamped gnss_pose;
+    LLH2UTM(msg_gps->LLH.x, msg_gps->LLH.y, msg_gps->LLH.z, gnss_pose_);
 
-    gnss_pose.header = msg_gps->header;
-    gnss_pose.header.frame_id = "/map";
+    gnss_pose_.pose.position.x = gnss_pose_.pose.position.x - x_offset_;
+    gnss_pose_.pose.position.y = gnss_pose_.pose.position.y - y_offset_;
 
-    LLH2UTM(msg_gps->LLH.x, msg_gps->LLH.y, msg_gps->LLH.z, gnss_pose);
-
-    gnss_pose.pose.position.x = gnss_pose.pose.position.x - x_offset_;
-    gnss_pose.pose.position.y = gnss_pose.pose.position.y - y_offset_;
-
-    double roll, pitch, yaw;
-
-    roll = msg_ins->YPR.z;
-    pitch = msg_ins->YPR.y;
+    roll_ = msg_ins->YPR.z;
+    pitch_ = msg_ins->YPR.y;
     
-    yaw = msg_ins->YPR.x;
-    yaw *= -1;
-    yaw -= yaw_offset_;
-    if(yaw > 180.0) yaw -= 360.0;
-    if(yaw < -180.0) yaw += 360.0;
+    yaw_ = msg_ins->YPR.x;
+    yaw_ *= -1;
+    yaw_ -= yaw_offset_;
+    if(yaw_ > 180.0) yaw_ -= 360.0;
+    if(yaw_ < -180.0) yaw_ += 360.0;
 
-    roll = roll * M_PI/180.0;
-    pitch = pitch * M_PI/180.0;
-    yaw = yaw * M_PI/180.0;
-
-    ToQuaternion(yaw, pitch, roll, gnss_pose.pose.orientation);
-
-    tf::Quaternion q;
-    q.setRPY(roll, pitch, yaw);
-    
-    tf::StampedTransform transform;
-    transform.setOrigin(tf::Vector3(gnss_pose.pose.position.x, gnss_pose.pose.position.y, 0.0));
-    transform.setRotation(q);
-    br.sendTransform(tf::StampedTransform(transform, msg_gps->header.stamp, "/map", "/base_link"));
-
-    gnss_pose_pub_.publish(gnss_pose);
+    roll_ = roll_ * M_PI/180.0;
+    pitch_ = pitch_ * M_PI/180.0;
+    yaw_ = yaw_ * M_PI/180.0;
 }
 
 int main(int argc, char *argv[]){
@@ -84,5 +68,24 @@ int main(int argc, char *argv[]){
 
     sync.registerCallback(boost::bind(&gnss_pose_pub_cb, _1, _2));
 
-    ros::spin();
+    tf::TransformBroadcaster br;
+    ros::Rate r(10);
+    while(nh.ok()){
+        ros::spinOnce();
+
+        ToQuaternion(yaw_, pitch_, roll_, gnss_pose_.pose.orientation);
+
+        tf::Quaternion q;
+        q.setRPY(roll_, pitch_, yaw_);
+
+        tf::StampedTransform transform;
+        transform.setOrigin(tf::Vector3(gnss_pose_.pose.position.x, gnss_pose_.pose.position.y, 0.0));
+        transform.setRotation(q);
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/map", "/base_link"));
+
+        gnss_pose_pub_.publish(gnss_pose_); 
+
+
+        r.sleep();
+    }
 }
