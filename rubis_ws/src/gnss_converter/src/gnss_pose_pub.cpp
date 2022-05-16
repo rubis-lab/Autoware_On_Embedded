0929156fc991fa3b0cf0469971874a958ea6fc3c
Sunho Kim <sunho001215@gmail.com>
2022-05-16 16:40:50 +0900

    update gnnss_pose_pub : publish msg considering /gnss to /base_link tf

--- rubis_ws/src/gnss_converter/src/gnss_pose_pub.cpp
+++ rubis_ws/src/gnss_converter/src/gnss_pose_pub.cpp
@@ -1,5 +1,4 @@
 #include <gnss_converter/LLH2UTM.h>
-#include <gnss_converter/quaternion_euler.h>
 
 #include <geometry_msgs/PoseStamped.h>
 #include <inertiallabs_msgs/gps_data.h>
@@ -7,12 +6,10 @@
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/sync_policies/exact_time.h>
 
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
-#include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
 
 #define M_PI 3.14159265358979323846
@@ -23,31 +20,31 @@ using namespace message_filters;
 typedef sync_policies::ExactTime<inertiallabs_msgs::gps_data, inertiallabs_msgs::ins_data> SyncPolicy_;
 
 static ros::Publisher gnss_pose_pub_;
+static geometry_msgs::PoseStamped gnss_pose_;
 static double x_offset_, y_offset_, yaw_offset_;
 static double roll_, pitch_, yaw_;
-static geometry_msgs::PoseStamped gnss_pose_;
 
 void gnss_pose_pub_cb(const inertiallabs_msgs::gps_data::ConstPtr &msg_gps, const inertiallabs_msgs::ins_data::ConstPtr &msg_ins){
     gnss_pose_.header = msg_gps->header;
     gnss_pose_.header.frame_id = "/map";
 
+    /* coordinate transform (LLH2 to UTM) */ 
     LLH2UTM(msg_gps->LLH.x, msg_gps->LLH.y, msg_gps->LLH.z, gnss_pose_);
 
+    /* position offset calculation */ 
     gnss_pose_.pose.position.x = gnss_pose_.pose.position.x - x_offset_;
     gnss_pose_.pose.position.y = gnss_pose_.pose.position.y - y_offset_;
 
+    /* orientation */ 
     roll_ = msg_ins->YPR.z;
     pitch_ = msg_ins->YPR.y;
-    
-    yaw_ = msg_ins->YPR.x;
-    yaw_ *= -1;
-    yaw_ -= yaw_offset_;
-    if(yaw_ > 180.0) yaw_ -= 360.0;
-    if(yaw_ < -180.0) yaw_ += 360.0;
 
-    roll_ = roll_ * M_PI/180.0;
-    pitch_ = pitch_ * M_PI/180.0;
-    yaw_ = yaw_ * M_PI/180.0;
+    /* yaw offset calculation */
+    yaw_ = -1 * (msg_ins->YPR.x) - yaw_offset_;
+    yaw_ = (yaw_ > 180.0) ? (yaw_ - 360) : ((yaw_ < -180) ? (yaw_ + 360) : yaw_);
+    
+    /* unit conversion */ 
+    roll_ = roll_ * M_PI/180.0; pitch_ = pitch_ * M_PI/180.0; yaw_ = yaw_ * M_PI/180.0;
 }
 
 int main(int argc, char *argv[]){
@@ -68,24 +65,52 @@ int main(int argc, char *argv[]){
 
     sync.registerCallback(boost::bind(&gnss_pose_pub_cb, _1, _2));
 
-    tf::TransformBroadcaster br;
-    ros::Rate r(10);
+    ros::Rate rate(10);
+
+    tf::TransformListener listener;
+    tf::TransformBroadcaster broadcaster;
+    tf::StampedTransform transform;
+    tf::Transform tf_gnss_to_base, tf_map_to_gnss, tf_map_to_base;
+    tf::Quaternion q;
+
+    while(nh.ok()){
+        try{
+            /* lookup /gnss to /base_link static transform */ 
+            listener.lookupTransform("/gnss", "/base_link", ros::Time(0), transform);
+            tf_gnss_to_base = transform;
+            break;
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("%s", ex.what());
+            rate.sleep();
+        }
+    }
+
     while(nh.ok()){
         ros::spinOnce();
-
-        ToQuaternion(yaw_, pitch_, roll_, gnss_pose_.pose.orientation);
-
-        tf::Quaternion q;
+        
+        /* /map to /gnss tf */ 
+        tf_map_to_gnss.setOrigin(tf::Vector3(gnss_pose_.pose.position.x, gnss_pose_.pose.position.y, gnss_pose_.pose.position.z));
         q.setRPY(roll_, pitch_, yaw_);
+        tf_map_to_gnss.setRotation(q);
 
-        tf::StampedTransform transform;
-        transform.setOrigin(tf::Vector3(gnss_pose_.pose.position.x, gnss_pose_.pose.position.y, 0.0));
-        transform.setRotation(q);
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/map", "/base_link"));
+        /* /map to /base tf calculation */ 
+        tf_map_to_base = tf_map_to_gnss * tf_gnss_to_base;
 
+        /* msg */  
+        gnss_pose_.pose.position.x = tf_map_to_base.getOrigin().x();
+        gnss_pose_.pose.position.y = tf_map_to_base.getOrigin().y();
+        gnss_pose_.pose.position.z = tf_map_to_base.getOrigin().z();
+
+        gnss_pose_.pose.orientation.w = tf_map_to_base.getRotation().w();
+        gnss_pose_.pose.orientation.x = tf_map_to_base.getRotation().x();
+        gnss_pose_.pose.orientation.y = tf_map_to_base.getRotation().y();
+        gnss_pose_.pose.orientation.z = tf_map_to_base.getRotation().z();
+
+        /* broadcast & publish */ 
+        broadcaster.sendTransform(tf::StampedTransform(tf_map_to_base, ros::Time::now(), "/map", "/base_link"));
         gnss_pose_pub_.publish(gnss_pose_); 
 
-
-        r.sleep();
+        rate.sleep();
     }
 }
