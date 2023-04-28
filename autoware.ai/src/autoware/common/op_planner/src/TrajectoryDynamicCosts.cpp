b6bef26266be43032491d98f5b1d2efa59a4b743
HoYongLee98 <dlghdydwkd79@gmail.com>
2023-04-28 15:02:45 +0900

    fix planner

--- autoware.ai/src/autoware/common/op_planner/src/TrajectoryDynamicCosts.cpp
+++ autoware.ai/src/autoware/common/op_planner/src/TrajectoryDynamicCosts.cpp
@@ -139,18 +139,18 @@ TrajectoryCost TrajectoryDynamicCosts::DoOneStepStatic(const vector<vector<WayPo
   PlanningHelpers::GetRelativeInfo(totalPaths, currState, car_info);
 
   // M
-  if(car_info.perp_point.LeftLnId == 100){
-    m_startTrajIdx = rollOuts.size() / 2;
-    m_endTrajIdx = rollOuts.size() / 2;
-  }
-  else if(car_info.perp_point.LeftLnId > car_info.perp_point.RightLnId){
-    m_endTrajIdx = min(car_info.perp_point.LeftLnId, int(rollOuts.size())-1);
-    m_startTrajIdx = min(car_info.perp_point.RightLnId, int(rollOuts.size())-1);
-  }
-  else{
-    m_startTrajIdx = min(car_info.perp_point.LeftLnId, int(rollOuts.size())-1);
-    m_endTrajIdx = min(car_info.perp_point.RightLnId, int(rollOuts.size())-1);
-  }
+  // if(car_info.perp_point.LeftLnId == 100){
+  //   m_startTrajIdx = rollOuts.size() / 2;
+  //   m_endTrajIdx = rollOuts.size() / 2;
+  // }
+  // else if(car_info.perp_point.LeftLnId > car_info.perp_point.RightLnId){
+  //   m_endTrajIdx = min(car_info.perp_point.LeftLnId, int(rollOuts.size())-1);
+  //   m_startTrajIdx = min(car_info.perp_point.RightLnId, int(rollOuts.size())-1);
+  // }
+  // else{
+  //   m_startTrajIdx = min(car_info.perp_point.LeftLnId, int(rollOuts.size())-1);
+  //   m_endTrajIdx = min(car_info.perp_point.RightLnId, int(rollOuts.size())-1);
+  // }
 
   double minDistanceToRollOut = 0;
   int currIndex = 0;
@@ -206,8 +206,8 @@ TrajectoryCost TrajectoryDynamicCosts::DoOneStepStatic(const vector<vector<WayPo
   bool bAllFree = true;
 
   // M
-  for(int ic = 0; ic < m_startTrajIdx; ic++) m_TrajectoryCosts.at(ic).bBlocked = true;
-  for(int ic = rollOuts.size() - 1; ic > m_endTrajIdx; ic--) m_TrajectoryCosts.at(ic).bBlocked = true;
+  // for(int ic = 0; ic < m_startTrajIdx; ic++) m_TrajectoryCosts.at(ic).bBlocked = true;
+  // for(int ic = rollOuts.size() - 1; ic > m_endTrajIdx; ic--) m_TrajectoryCosts.at(ic).bBlocked = true;
 
   // for(unsigned int ic = std::max(currIndex - 1, m_startTrajIdx); ic <= std::min(currIndex + 1, m_endTrajIdx); ic++)
   // for(unsigned int ic = m_startTrajIdx; ic <= m_endTrajIdx; ic++)
